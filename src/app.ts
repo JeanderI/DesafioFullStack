712b2a4854e63eb7a29d70e7ceb5365fc552f8af
isac <jeandersantos0310@gmail.com>
2023-05-26 05:25:54 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import "express-async-errors";
 import express, { Application } from "express";
 import { handleErrors } from "./errors";
+import cors from "cors";
 import clientRoutes from "./routers/client.routes";
 import contactRoutes from "./routers/contact.routes";
 import loginRoutes from "./routers/login.routes";
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
-
 app.use("/clients", clientRoutes);
 app.use("/contacts", contactRoutes);
 app.use("/login", loginRoutes);
